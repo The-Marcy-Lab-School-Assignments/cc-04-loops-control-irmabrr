16d4532d307fae87fd5d1e143ac1371e0b94709d
Irma <barrios12214@gmail.com>
2024-10-08 11:05:13 -0400

    added to 5

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-// //Write your solutions in this file, don't forget to test your functions.
+//Write your solutions in this file, don't forget to test your functions.
 
-// function countFromOne (num) {
-//             for (let i = 1; num >= i; i++){
-//                         console.log(i);
-//             }
-// }
+function countFromOne(num) {
+  for (let i = 1; num >= i; i++) {
+    console.log(i);
+  }
+}
 
-// countFromOne(10);
+countFromOne(10);
 
-// function countEveryOdd (int) {
-//             for (let j =1; int >= j; j+=2){
-//                         console.log(j)
-//             }
-// }
-// countEveryOdd(10);
+function countEveryOdd(int) {
+  for (let j = 1; int >= j; j += 2) {
+    console.log(j);
+  }
+}
+countEveryOdd(10);
 
-// function isNegative(arg){
-//             if (arg <= 0){
-//                         return true;
-//             } else {
-//                         return false;
-//             }
-// }
-// console.log(isNegative(Math.PI));
+function isNegative(arg) {
+  if (arg <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isNegative(Math.PI));
 
-// function betweenFiveAndTwenty (para){
-//             if (para >= 5 && para <= 20){
-//                         return true;
-//             } else {
-//                         return false;
-//             }
-// }
-// console.log (betweenFiveAndTwenty(100));
+function betweenFiveAndTwenty(para) {
+  if (para >= 5 && para <= 20) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(betweenFiveAndTwenty(100));
 
 function sumOfThreeOrFive() {
+  sum = 0;
   for (i = 1; i > 1000; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-      i = j;
-      j = k;
-      sum = j + k;
+      sum = sum + i;
     }
   }
 }
