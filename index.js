702d9da49b98dfa24a66c746bc9c8c588b2f7727
Irma <barrios12214@gmail.com>
2024-10-08 10:57:30 -0400

    finished till question 5, very stuck

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-//Write your solutions in this file, don't forget to test your functions.
+// //Write your solutions in this file, don't forget to test your functions.
+
+// function countFromOne (num) {
+//             for (let i = 1; num >= i; i++){
+//                         console.log(i);
+//             }
+// }
+
+// countFromOne(10);
+
+// function countEveryOdd (int) {
+//             for (let j =1; int >= j; j+=2){
+//                         console.log(j)
+//             }
+// }
+// countEveryOdd(10);
+
+// function isNegative(arg){
+//             if (arg <= 0){
+//                         return true;
+//             } else {
+//                         return false;
+//             }
+// }
+// console.log(isNegative(Math.PI));
+
+// function betweenFiveAndTwenty (para){
+//             if (para >= 5 && para <= 20){
+//                         return true;
+//             } else {
+//                         return false;
+//             }
+// }
+// console.log (betweenFiveAndTwenty(100));
+
+function sumOfThreeOrFive() {
+  for (i = 1; i > 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      i = j;
+      j = k;
+      sum = j + k;
+    }
+  }
+}
+sumOfThreeOrFive();
+
+function isAllLowerCase (' '){
+            if ( ){
+                        return true;
+            } else {
+                        return false;
+            }
+
+}
